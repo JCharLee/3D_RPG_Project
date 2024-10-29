f0c81880617b59533641a35319d2e2deb500bdac
JaeSung Lee <wotjd999@gmail.com>
2024-10-29 10:30:21 +0900

    프로젝트 초기 업로드
    첫 업로드(초기)

---
+++ Assets/02.Scripts/PlayerInput.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    [Header("Keyboard")]
+    public Vector2 move;
+    public Vector2 look;
+    public bool jump;
+
+    [Header("Mouse")]
+    public bool leftClick;
+    public bool rightClick;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
