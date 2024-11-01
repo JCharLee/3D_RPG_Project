caed81d8ea3a6e32fd29a3a9757c099d9216545b
JCharLee <wotjd999@gmail.com>
2024-11-01 20:04:51 +0900

    인풋 시스템, 스크립트
    인풋 시스템 생성.
    PlayerInputController 스크립트 생성.

---
+++ Assets/02.Scripts/PlayerInputController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInputController : MonoBehaviour
+{
+    public Vector2 move;
+    public Vector2 look;
+    public bool jump;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
